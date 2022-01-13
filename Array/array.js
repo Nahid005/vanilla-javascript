46ed5a871a707a6fb839eb92e69fa227a0e35024
Nahid005 <nahidhassan9258@gmail.com>
2022-01-13 09:17:00 +0600

    object practise

--- Array/array.js
+++ Array/array.js
@@ -5,14 +5,39 @@
 // array length calculation 
 
 let car = ["bmw","oudi","toyota","tesla"];
-carLength = car.length;
+// carLength = car.length;
 
-console.log(carLength)
+// console.log(carLength)
 
 // array position change 
 
 // let car = [bmw, audi, toyota, tesla];
 
-console.log(car)
+// console.log(car)
 
-car[0] = "nahid";
+// car[0] = "nahid";
+
+// console.log(car);
+
+// car.push("nahid")
+
+// console.log(car);
+
+// car.pop();
+// console.log(car);
+
+// car.pop();
+// console.log(car);
+
+// car.splice(1,2);
+// console.log(car);
+
+// car.indexOf();
+// console.log(car);
+
+// car.length;
+// console.log(car);
+
+for(var i = 1 ; i< car.length; i++){
+    console.log("Hello Array");
+}
