c23531198fc29c7c00f0c94d19e2ea8cef2e0991
Nahid005 <nahidhassan9258@gmail.com>
2022-01-19 00:51:54 +0600

    dom manipulation

---
+++ dom/dom.js
@@ -0,0 +1,13 @@
+var authorList = document.getElementsByClassName('author');
+for(var i = 0; i < authorList.length; i++) {
+    var eliment = authorList[i];
+    eliment.style.color = "green";
+    // console.log(eliment.innerHTML);
+    // eliment.innerHTML = " Nahid Hassan ";
+}
+
+var title = document.getElementById('sectionTitle');
+var newEliment = document.createElement("p");
+newEliment.innerHTML =" Hello Nahid Hassan" ;
+
+title.appendChild(newEliment);
